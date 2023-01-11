c8ec04ee3749ee62710851d92c03012a6b82f218
Naidenov1898 <naiden1898@yandex.ru>
2023-01-11 16:10:48 +0300

    new code

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,8 @@ from api.models import (Cart, Favorite, Ingredient, IngredientAmount, Recipe,
 from api.pagination import LimitPageNumberPagination
 from api.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from api.serializers import (CropRecipeSerializer, IngredientSerializer,
-                             RecipeSerializer, TagSerializer, RecipesListSerializer)
+                             RecipeSerializer, TagSerializer, 
+                             RecipesListSerializer)
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from reportlab.pdfbase import pdfmetrics
